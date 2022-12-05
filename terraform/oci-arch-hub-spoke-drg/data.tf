3bcc823881b7915b7d527a8abee2cf3089641c5c
Haroldo Proenca <haroldoproenca@gmail.com>
2022-12-05 10:56:40 -0300

    Cleanup unused repos

--- terraform/oci-arch-hub-spoke-drg/data.tf
+++ terraform/oci-arch-hub-spoke-drg/data.tf
@@ -6,20 +6,6 @@ data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
 
-# Get the latest Oracle Linux image
-data "oci_core_images" "InstanceImageOCID" {
-  compartment_id           = var.compartment_ocid
-  operating_system         = var.instance_os
-  operating_system_version = var.linux_os_version
-  shape                    = var.InstanceShape
-
-  filter {
-    name   = "display_name"
-    values = ["^.*Oracle[^G]*$"]
-    regex  = true
-  }
-}
-
 data "oci_identity_region_subscriptions" "home_region_subscriptions" {
   tenancy_id = var.tenancy_ocid
 
